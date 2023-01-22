d330ceb9310fc125db679c3fe8347ee0dae3ac3b
Sunnysingh1231 <123saktisingh@gmail.com>
2023-01-22 22:13:16 +0530

    Day-5

--- paymentpage.js
+++ paymentpage.js
@@ -2,4 +2,5 @@ let payconf=document.getElementById("payconf")
 console.log(payconf);
 payconf.addEventListener("click",()=>{
     alert("Payment successful")
+    window.location .href="./index.html"
 })
